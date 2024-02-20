4359b047fe3b1e79c7adff1cd2b2e5be82409b33
Vlad Marii <mariyvladyslav@gmail.com>
2024-02-20 20:25:19 +0200

    final back

---
+++ src/main/java/com/example/angularspringconnect/model/Employee.java
@@ -0,0 +1,27 @@
+package com.example.angularspringconnect.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCod;
+
+}
